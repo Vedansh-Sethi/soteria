64fecdbdf369178fdafaf9e725832da4e3348ac2
Vedansh Sethi <vedansh25sethi@gmail.com>
2026-05-03 08:02:08 +0530

    Big Integer handling offloaded to boost library

---
+++ src/utils/uint256/uint256.hpp
@@ -0,0 +1,5 @@
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/fwd.hpp>
+
+using uint256 = boost::multiprecision::uint256_t;
+using uint512 = boost::multiprecision::uint512_t;
