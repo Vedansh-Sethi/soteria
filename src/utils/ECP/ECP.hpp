7e14402f50838410809d1205aba536dc575fab44
Vedansh Sethi <vedansh25sethi@gmail.com>
2026-05-03 12:58:59 +0530

    Fixed initialization bug in ECP for Finite Field elements

--- src/utils/ECP/ECP.hpp
+++ src/utils/ECP/ECP.hpp
@@ -15,29 +15,31 @@ template <typename T> class GenericECP
     std::optional<T> y;
 
   public:
-    GenericECP(T a, T b, std::optional<T> x, std::optional<T> y)
+    GenericECP(const T &a, const T &b, const std::optional<T> &x,
+                  const std::optional<T> &y) : a(a), b(b), x(std::nullopt), y(std::nullopt)
     {
-        if (!x.has_value() && !y.has_value()) return;
+        if (!x.has_value() && !y.has_value())
+            return;
         T xVal = x.value();
         T yVal = y.value();
-        if (yVal * yVal != xVal*xVal*xVal + a*xVal + b)
+        if (yVal * yVal != xVal * xVal * xVal + a * xVal + b)
         {
-            throw std::invalid_argument("Points do not lie on elliptic curve");
+            throw std::invalid_argument("point does not lie on elliptic curve");
         }
-        this->a = a;
-        this->b = b;
-        this-> x = xVal;
-        this->y = yVal;
+        this->x = x;
+        this->y = y;
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const GenericECP<T>& ECP)
+    friend std::ostream &operator<<(std::ostream &os, const GenericECP<T> &ECP)
     {
-        if(!ECP.x.has_value())
+        if (!ECP.x.has_value())
         {
             return os << "ECP(Infinity)_" << ECP.a << "_" << ECP.b;
         }
-        return os << "ECP(" << ECP.x << "," << ECP.y << ")_" << ECP.a << "_" << ECP.b << ")";
+        return os << "ECP(" << ECP.x.value() << "," << ECP.y.value() << ")_"
+                  << ECP.a << "_" << ECP.b << ")";
     }
+
     bool operator==(const GenericECP<T> &other) const;
     bool operator!=(const GenericECP<T> &other) const;
     GenericECP<T> operator+(const GenericECP<T> &other) const;
