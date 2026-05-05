77b42b017b4c65d08f26d24cb90deed675c5e6ce
Vedansh Sethi <vedansh25sethi@gmail.com>
2026-05-05 21:38:50 +0530

    Created hexify and dehexiify functions to deal with hashes

---
+++ src/utils/hexer/hexer.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "utils/uint256/uint256.hpp"
+#include <algorithm>
+#include <cctype>
+#include <string>
+
+std::string hexify(uint256 num)
+{
+    std::string hexer = "0123456789abcdef";
+    std::string hexed;
+    for(int i = 0;i < 64; i++)
+    {
+        hexed += hexer[int(num & 0xf)];
+        num >>= 4;
+    }
+    std::reverse(hexed.begin(),  hexed.end());
+    hexed = "0x" + hexed;
+    return hexed;
+}
+
+uint256 dehexify(std::string hex)
+{
+    uint256 dehexed = 0;
+    for(size_t i = 2; i < hex.size(); i++)
+    {
+        if(std::isdigit(hex[i]))
+        {
+            uint256 bitShift = uint256(1)<<((hex.size() - i - 1)*4);
+            dehexed += (hex[i] - '0')*bitShift;
+        }
+        if('a' <= hex[i] && hex[i] <= 'f')
+        {
+            uint256 bitShift = uint256(1)<<((hex.size() - i - 1)*4);
+            dehexed += (hex[i] - 'a' + 10)*bitShift;
+        }
+    }
+    return dehexed;
+}
