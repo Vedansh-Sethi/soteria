b80dc8ea6f16f4cbb1675b2d790e548a30791587
Vedansh Sethi <vedansh25sethi@gmail.com>
2026-05-07 15:07:16 +0530

    Signature object to transfer proofs

---
+++ src/signing/signature/signature.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ECC/secp256k1/S256Field/S256Field.hpp"
+#include "signing/scalarField/scalarField.hpp"
+class Signature
+{
+  public:
+    S256Field r;
+    ScalarField s;
+    
+    friend std::ostream& operator<<(std::ostream& os, const Signature& sign)
+    {
+        os << "Signature(" << sign.r << "," << sign.s << ")";
+        return os;
+    }
+    
+};
