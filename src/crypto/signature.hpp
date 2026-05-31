c01a406b8fe638e894b83660f2632c4cbcafde56
Vedansh Sethi <vedansh25sethi@gmail.com>
2026-05-31 13:45:04 +0530

    Added Private Key constructor

--- src/crypto/signature.hpp
+++ src/crypto/signature.hpp
@@ -24,10 +24,11 @@ class PrivateKey
 {
   private:
     ScalarField secret;
-    S256Point publicKey = Genesis * secret.data;
 
   public:
     static std::string RNG256();
-    std::pair<S256Field, ScalarField> sign(uint256 z) const;
+    S256Point publicKey = Genesis * secret.data;
+    Signature sign(uint256 z) const;
     std::array<std::byte, 34> wif(bool testnet = false) const;
+    PrivateKey(ScalarField secret) : secret(secret) {}
 };
